a2ead8d01fffee4ae11ac31620c95b8392ba96f4
Borba Business <69469835+borba-business@users.noreply.github.com>
2021-10-22 19:48:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+//Para criar novos cartões apenas alterar os nomes dos usuarios a baixo no "const LinksSocialMedia"
+
+const LinksSocialMedia = {
+  github: 'borba-business',
+  youtube: 'NvZer0',
+  facebook: 'jaziel.borba',
+  instagram: 'jazi_borba',
+  twitter: 'JazielBorba'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
